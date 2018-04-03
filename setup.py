82628328ef2590f2774143d437efb676c65146ab
MelvinLikeABird <melvin@likeabird.io>
2018-04-03 17:32:33 +0200

    Delete pip parse requirements

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 #from distutils.core import setup
-import re, uuid
+import re
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 VERSIONFILE = "tweepy/__init__.py"
 ver_file = open(VERSIONFILE, "rt").read()
@@ -14,9 +13,6 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
-install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
-reqs = [str(req.req) for req in install_reqs]
-
 setup(name="tweepy",
       version=version,
       description="Twitter library for python",
@@ -25,7 +21,11 @@ setup(name="tweepy",
       author_email="tweepy@googlegroups.com",
       url="http://github.com/tweepy/tweepy",
       packages=find_packages(exclude=['tests']),
-      install_requires=reqs,
+      install_requires=[
+        'requests>=2.4.3',
+        'requests_oauthlib>=0.4.1',
+        'six>=1.7.3'
+      ],
       keywords="twitter library",
       classifiers=[
           'Development Status :: 4 - Beta',
